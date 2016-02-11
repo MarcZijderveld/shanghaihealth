caaf8cf38373e157f43dd0e8fe2c90b6842f6203
mentosmenno2 <mentosmenno2@hotmail.com>
2016-02-11 13:02:15 +0100

    add home js

--- app/views/home.js
+++ app/views/home.js
@@ -1,3 +1,42 @@
 if(Meteor.isClient) {
 
+	var random = false;
+
+	$(document).keypress(function(event) {
+		console.log(event.keyCode);
+		if (event.keyCode == 13) {
+			console.log('Enter was pressed');
+		}
+
+		if (event.keyCode == 49) {
+			$("#heartRate").text(75);
+		}
+		else if (event.keyCode == 50) {
+			$("#heartRate").text(85);
+		}
+		else if (event.keyCode == 51) {
+			$("#heartRate").text(110);
+		}
+		else if (event.keyCode == 52) {
+			if (random) {
+				random = false;
+				console.log("disable auto refresh");
+			}
+			else {
+				random = true;
+				console.log("enable auto refresh");
+			}
+		}
+	});
+
+	refresh();
+
+	function refresh() {
+		if (random == true) {
+			var rand = Math.floor(Math.random() * 85) + 75;
+			$("#heartRate").text(rand);
+			console.log("refresh naar " + rand);
+		}
+		setTimeout(refresh, 3000);
+	}
 }
